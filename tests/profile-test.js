731a0acb83c8d1109bc492ef5b65446840ed2358
Alex Navasardyan <alex.navasardyan@yahoo.com>
2013-12-23 01:09:21 -0500

    final fixes for console api

--- tests/profile-test.js
+++ tests/profile-test.js
@@ -1,12 +1,12 @@
-// var fs = require("fs"),
-    // benchmarker = require("../benchmarker");
+var fs = require("fs"),
+    benchmarker = require("../benchmarker");
 
-// exports['should convert plain javascript to consoleAPI suite'] = function(test) {
-  // var source   = fs.readFileSync(__dirname + "/fixtures/test-benchmark.js").toString(),
-      // expected = fs.readFileSync(__dirname + "/fixtures/profile/end-to-end-benchmark.out.js").toString(),
-      // res      = benchmarker(source, { type: 'consoleapi' });
+exports['should convert plain javascript to consoleAPI suite'] = function(test) {
+  var source   = fs.readFileSync(__dirname + "/fixtures/test-benchmark.js").toString(),
+      expected = fs.readFileSync(__dirname + "/fixtures/profile/end-to-end-benchmark.out.js").toString(),
+      res      = benchmarker(source, { type: 'consoleapi' }) + '\n';
 
-  // test.expect(1);
-  // test.equal(res, expected);
-  // test.done();
-// };
+  test.expect(1);
+  test.equal(res, expected);
+  test.done();
+};
